c1deeb715f8e07201fd389ef1d974338b6683392
Anthony Garrett <tony.garrett@gmail.com>
2022-11-27 17:15:03 -0500

    back to work

---
+++ javascript/level_7/11_27_22.js
@@ -0,0 +1,6 @@
+// args_count(1, 2, 3) -> 3
+// args_count(1, 2, 3, 10) -> 4
+// Create a function called args_count that returns the number of arguments provided
+function args_count(...args) {
+  return arguments.length;
+}
