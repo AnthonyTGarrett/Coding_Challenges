1f21a912636a58a680b0186e81957ebfc6dac73d
Tony Garrett <tony.garrett@gmail.com>
2023-10-02 17:57:21 -0400

    first question done

--- python/level_8/10_02_23.py
+++ python/level_8/10_02_23.py
@@ -7,3 +7,15 @@
 # Examples
 # count_by(1,10) #should return [1,2,3,4,5,6,7,8,9,10]
 # count_by(2,5) #should return [2,4,6,8,10]
+def count_by(x, n):
+    """
+    Return a sequence of numbers counting by `x` `n` times.
+    """
+    result = []
+    num = x
+    for i in range(n):
+        result.append(num)
+        num += x
+        
+    return result
+        
