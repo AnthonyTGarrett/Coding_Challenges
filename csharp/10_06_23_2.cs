c0a36e6c727426f3dc3044d8c613c59a77d463e2
Tony Garrett <tony.garrett@gmail.com>
2023-10-06 20:09:02 -0400

    Two down on the first day yo

--- csharp/10_06_23_2.cs
+++ csharp/10_06_23_2.cs
@@ -7,3 +7,22 @@
 // on is the number of people on the bus excluding the driver.
 // wait is the number of people waiting to get on to the bus excluding the driver.
 // If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+using System;
+
+public static class Kata
+{
+  public static int Enough(int cap, int on, int wait)
+  {
+    int peopleTaken = cap - on;
+
+    if (peopleTaken >= wait)
+    {
+      return 0;
+    }
+    if (peopleTaken < wait)
+    {
+      return wait - peopleTaken;
+    }
+    return 0;
+  }
+}
