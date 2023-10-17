876247f37c408dd81496570e087931a8a39f504f
Tony Garrett <tony.garrett@gmail.com>
2023-10-16 20:23:45 -0400

    Fixes

--- csharp/10_11_23.cs
+++
@@ -1,9 +0,0 @@
-// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
-
-public class Kata
-{
-  public static string SayHello(string name)
-  {
-    return $"Hello, {name}";
-  }
-}
